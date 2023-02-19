c26ed688b28fe5a99f346382b735cf6b055583ba
Mwende-Fifi <mwendeeeekiindu@gmail.com>
2023-02-19 01:50:24 -0800

    ./101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,26 +18,25 @@ int main(void)
 	for (d = '0'; d < '9'; d++)
 	{
 
-	for (p = d + 1; p <= '9'; p++)
-	{
-	
-	for (q = p + 1; q <= '9'; q++)
-	{
+		for (p = d + 1; p <= '9'; p++)
+		{
+			for (q = p + 1; q <= '9'; q++)
+			{
 
-	if ((p != d) != q)
-	{
-	putchar(d);
-	putchar(p);
-	putchar(q);
-	if (d == '7' && p == '8')
-	continue;
-	putchar(',');
-	putchar(' ');
+				if ((p != d) != q)
+				{
+					putchar(d);
+					putchar(p);
+					putchar(q);
+					if (d == '7' && p == '8')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
-	}
-	}
-        }
-        putchar('\n');
+	putchar('\n');
 	return (0);
 }
 
