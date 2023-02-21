f783e341c12dc0e1a7f89caca3fe66b73a1e18e9
Mwende-Fifi <mwendeeeekiindu@gmail.com>
2023-02-21 02:37:01 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,7 @@
 
 int main(void)
 
-{	
-	_putchar('_');
+{	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -17,8 +16,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
-
+	return (0);
 }	
 
 
