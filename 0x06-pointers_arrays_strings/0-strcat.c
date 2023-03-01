6976d19176add8d422140ad8397ad3b70ad4140c
Mwende-Fifi <mwendeeeekiindu@gmail.com>
2023-03-01 07:26:49 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,15 @@
-#include "main.h"
+
+
+
+
+
+
+
+
+
+
+
+
 
 /**
  * _strcat -concatenates two strings
@@ -10,24 +21,20 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-	
-	i=0;
+
+	i = 0;
 	while (dest[i] != '\0')
 	{
-	    i++;
-
+		i++;
 	}
-	j=0;
+	j = 0;
 	while (src[j] != '\0')
 	{
-		
-	    dest[i] = src[j];
-	    i++;
-	    j++;
-
-	}	
-	
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	dest[i] = '\0';
 	return (dest);
 }
-	
+
