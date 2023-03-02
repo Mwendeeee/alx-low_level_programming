f7b254a6972252e1d95af26f1e376a7f001a26f3
Mwende-Fifi <mwendeeeekiindu@gmail.com>
2023-03-02 05:03:09 -0800

    6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,8 +3,9 @@
 
 
 
+
 /**
- * cap_string -Capitalizes all words of a string.
+ * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
  * Return: A pointer to the changed string.
  */
