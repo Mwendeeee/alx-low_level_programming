eda244634cebe3e48048f5a76017e8f3ae3a1f5d
Mwende-Fifi <mwendeeeekiindu@gmail.com>
2023-03-04 04:49:43 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,27 +3,18 @@
 
 
 
-
-
-
-
-
-
-
-
 /**
  * print_alphabet - Make the alphabet
  * Return: void
  */
 
 void print_alphabet(void)
-{
-
+{	
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-	putchar(c);
+	_putchar(c);
 	}
-	putchar('\n');
+	_putchar('\n');
 }	
